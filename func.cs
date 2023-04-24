a9942b3a6ced27f9248ba36638fd80dc9791fd57
opilane <opilane@HP-CZC2349HTB>
2023-04-24 10:54:29 +0300

    Add project files.

---
+++ func.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KordamineIseseisevToo
+{
+    public class func
+    {
+        public static string Palindrom (string vastus)
+        {
+            string answer;
+            string vastwithout = vastus.Replace(" ","");
+            char[] chars = vastwithout.ToCharArray();
+            Array.Reverse(chars);
+            string chars1 = chars.ToString();
+            //bool result = vastwithout == chars1;
+            //Console.WriteLine(result);
+            Console.WriteLine(chars);
+            Console.WriteLine(vastwithout);
+            if (vastwithout == chars1) { answer = (vastus+" on polindromi sona!"); }
+            else { answer = (vastus + " ei ole polindromi sona!"); }
+            return answer;
+        }
+        
+    }
+}
---
+++ start.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KordamineIseseisevToo
+{
+    public class start
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Sisesta palindromi sõna:");
+            string sonavas = Console.ReadLine();
+            Console.WriteLine(func.Palindrom(sonavas));
+
+        }
+    }
+}
